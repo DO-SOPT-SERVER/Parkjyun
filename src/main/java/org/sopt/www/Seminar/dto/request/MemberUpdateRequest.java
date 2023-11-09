a8a4194f017c4a1acf87629a9facf7e08125a672
Parkjyun <jaeyun0507@naver.com>
2023-11-09 15:43:53 +0900

    [feat]3주차 코드 추가

--- src/main/java/org/sopt/www/Seminar/dto/request/MemberUpdateRequest.java
+++
@@ -1,7 +0,0 @@
-package org.sopt.www.Seminar.dto.request;
-
-import org.sopt.www.Seminar.domain.Member;
-import org.sopt.www.Seminar.domain.Part;
-
-public record MemberUpdateRequest(int generation, Part part) {
-}
