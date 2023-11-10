93f09cdd5635d6d4e8bc688d828473dae2a3739c
Parkjyun <jaeyun0507@naver.com>
2023-11-10 11:46:56 +0900

    [feat]member post 양방향 연관관계 설정

--- src/main/java/org/sopt/www/Seminar/domain/Member.java
+++ src/main/java/org/sopt/www/Seminar/domain/Member.java
@@ -7,6 +7,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.sopt.www.Seminar.dto.request.member.MemberUpdateRequest;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)//protected인 기본 생성자를 만든다. @transaction은 proxy객체를 활용하는데 proxy객체 위해 protected이상의 생성자 필
@@ -23,6 +26,9 @@ public class Member extends BaseTimeEntity {
     //공통적으로 관리할 수 있는 필드들은 한 객체의 속성으로 보고 객체로 관리
     //임베디드를 붙이면 나중에 데이터베이스에 sopt의 필드들이 멤버의 필드들로 들어감
     private Sopt sopt;
+
+    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
+    private final List<Post> posts = new ArrayList<>();
 
     @Builder
     public Member(String name, String nickname, int age, Sopt sopt) {//아이디빼고 생성자를 만듦 왜와이? pk는 generatedvalue로 자동으로 만들어주기에
